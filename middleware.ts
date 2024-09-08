da438285ea3dca890061eb001b88b00268414e00
abhicody <honeydude009@gmail.com>
2024-09-08 13:32:04 +0530

    routejs change to  original

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
-const publicRoutes = ["/api/webhooks/clerk"];
 const ignoredRoutes = ["/api/webhooks/clerk"];
 
-// export default clerkMiddleware((auth, request) => {
-//   if (!isPublicRoute(request) && !ignoredRoutes.includes(request.url)) {
-//     auth().protect();
-//   }
-// });
-
 export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
+  if (!isPublicRoute(request) && !ignoredRoutes.includes(request.url)) {
     auth().protect();
   }
-  publicRoutes;
-  ignoredRoutes;
 });
 
 export const config = {
@@ -26,6 +17,35 @@ export const config = {
     "/(api|trpc)(.*)",
   ],
 };
+
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+// const publicRoutes = ["/api/webhooks/clerk"];
+// const ignoredRoutes = ["/api/webhooks/clerk"];
+
+// // export default clerkMiddleware((auth, request) => {
+// //   if (!isPublicRoute(request) && !ignoredRoutes.includes(request.url)) {
+// //     auth().protect();
+// //   }
+// // });
+
+// export default clerkMiddleware((auth, request) => {
+//   if (!isPublicRoute(request)) {
+//     auth().protect();
+//   }
+//   publicRoutes;
+//   ignoredRoutes;
+// });
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
 
 // import rateLimit from "express-rate-limit";
 
